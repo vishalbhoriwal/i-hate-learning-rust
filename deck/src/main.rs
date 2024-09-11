bb1ff88b5aedd9f7e026891ebc70de56c05694a5
Vishal Bhoriwal <vishal.bhoriwal@cimpress.com>
2024-09-11 13:43:22 +0530

    update rand in package

--- deck/src/main.rs
+++ deck/src/main.rs
@@ -1,10 +1,41 @@
+use rand::{ thread_rng, seq::SliceRandom};
 #[derive(Debug)]
 struct Deck {
     cards: Vec<String>,
 }
 
+impl Deck {
+    fn new() -> Self {
+        let suits = ["Hearts", "Diamonds", "Clubs", "Spades"];
+        let values = ["Ace", "Two", "Three" ];
+    
+        let mut cards = vec![];
+    
+        for suit in suits { 
+            for value in values {
+                let card = format!("{value} of {suit}");
+                cards.push(card);
+            }
+        }
+
+        Deck { cards }
+    }
+    
+    fn shuffle(&mut self) {
+        let mut rng = thread_rng();
+        self.cards.shuffle(&mut rng);
+    }
+
+    fn deal(&mut self, num_cards: usize) -> Vec<String> {
+        self.cards.split_off(self.cards.len() - num_cards)
+    }
+}
+
 fn main() {
-    let deck = Deck { cards : Vec::new() };
-    println!("Here is your deck: {:?}", deck);
+    let mut deck = Deck::new();
+    // deck.shuffle();
+    let cards = deck.deal(90);
+    println!("Here are your cards: {:?}", cards);
+    // println!("Here is your deck: {:#?}", deck);
 }
 
